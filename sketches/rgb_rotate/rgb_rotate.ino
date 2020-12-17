9dc98738c5d52c34a5713f2e40b4edb20fea91ab
tobywan <wxyz4321@hotmail.com>
2020-12-17 19:44:58 +0000

    Work through all combinations. Slowly

--- sketches/rgb_rotate/rgb_rotate.ino
+++ sketches/rgb_rotate/rgb_rotate.ino
@@ -13,21 +13,34 @@ void setup()
 
 void loop()
 {
-  fade(5, RED);
-  fade(10, GREEN);
-  fade(20, BLUE);
+  cycle(5);
+}
+
+void setColor(byte red, byte green, byte blue)
+{
+  analogWrite(RED, red);
+  analogWrite(GREEN, green);
+  analogWrite(BLUE, blue);
+}
+
+void cycle(unsigned long dwMs)
+{
+  for (int r = 0; r < 255; r++)
+  { // loop from 0 to 254 (fade in)
+    for (int g = 0; g < 255; g++)
+    { // loop from 0 to 254 (fade in)
+      for (int b = 0; b < 255; b++)
+      { // loop from 0 to 254 (fade in)
+      setColor(r,g,b);
+      delay(dwMs);
+      }
+    }
+  }
 }
 
 void fade(int delayms, int color)
 {
 
-  for (int i = 0; i < 255; i++)
-  {                        // loop from 0 to 254 (fade in)
-    analogWrite(color, i); // set the LED brightness
-    delay(delayms);        // Wait 10ms because analogWrite
-    // is instantaneous and we would
-    // not see any change
-  }
   for (int i = 255; i > 0; i--)
   {                        // loop from 255 to 1 (fade out)
     analogWrite(color, i); // set the LED brightness
